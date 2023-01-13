56f5a40c3c2e6b0907b8580c138dba24f7e49b56
Andrey Ivanov <prioritet2503@gmail.com>
2023-01-13 02:10:30 +0200

    add parth logic

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -1,2 +1,41 @@
-import Stripe from "stripe";
-const stripe = new Stripe();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    console.log(req.body.cartItems);
+    try {
+      const params = {
+        submit_type: "pay",
+        mode: "payment",
+        payment_method_types: ["card"],
+        billing_address_collection: "auto",
+        shipping_options: [
+          { shipping_rate: "shr_1MPZvjAyLTz7OLpgUIcTvFlS" },
+          { shipping_rate: "shr_1MPZxKAyLTz7OLpgfTcMMatM" },
+        ],
+
+        line_items: req.body.cartItems.map((item) => {
+          const img = item.image[0].asset._ref;
+          const newImage = img
+            .replace(
+              "image-",
+              "https://cdn.sanity.io/images/cii268xn/production/"
+            )
+            .replace("-wbp", ".webp");
+          console.log("IMAGE", newImage);
+        }),
+        mode: "payment",
+        success_url: `${req.headers.origin}/?success=true`,
+        cancel_url: `${req.headers.origin}/?canceled=true`,
+      };
+      // Create Checkout Sessions from body params.
+      const session = await stripe.checkout.sessions.create(params);
+      res.redirect(303, session.url);
+    } catch (err) {
+      res.status(err.statusCode || 500).json(err.message);
+    }
+  } else {
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method Not Allowed");
+  }
+}
