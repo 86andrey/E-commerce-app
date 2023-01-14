9515df05da2361a0cfe87205389961103905d465
Andrey Ivanov <prioritet2503@gmail.com>
2023-01-15 00:35:56 +0200

    add payment page

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -1,20 +1,18 @@
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+import Stripe from "stripe";
+const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
-    console.log(req.body.cartItems);
+    console.log(req.body);
     try {
       const params = {
         submit_type: "pay",
         mode: "payment",
         payment_method_types: ["card"],
         billing_address_collection: "auto",
-        shipping_options: [
-          { shipping_rate: "shr_1MPZvjAyLTz7OLpgUIcTvFlS" },
-          { shipping_rate: "shr_1MPZxKAyLTz7OLpgfTcMMatM" },
-        ],
+        shipping_options: [{ shipping_rate: "shr_1MQI7KAyLTz7OLpgtqwFu6Zv" }],
 
-        line_items: req.body.cartItems.map((item) => {
+        line_items: req.body.map((item) => {
           const img = item.image[0].asset._ref;
           const newImage = img
             .replace(
@@ -44,7 +42,7 @@ export default async function handler(req, res) {
       };
       // Create Checkout Sessions from body params.
       const session = await stripe.checkout.sessions.create(params);
-      res.redirect(303, session.url);
+      res.status(200).json(session);
     } catch (err) {
       res.status(err.statusCode || 500).json(err.message);
     }
